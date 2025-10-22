16140cd6855d4f1c709e12cf19fd1023703d5d56
Khonsa <khonsaazmi@gmail.com>
2025-10-23 01:33:14 +0700

    Add DOM Elements

---
+++ script.js
@@ -0,0 +1,8 @@
+// ROCK PAPER SCISSORS
+
+// DOM Elements
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementaById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
